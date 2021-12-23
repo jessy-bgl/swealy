f8ad15ff3a92a7154bd322980d2a3ee9b47f69e2
Jessy BOGALHO <j.bogalho@m2mfactory.fr>
2021-12-23 14:12:39 +0100

    nestjs schedule module added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { HttpCustomModule } from './infrastructure/config/axios/http.module';
 import { DatabaseModule } from './infrastructure/config/mongoose/database.module';
 import { ControllersModule } from './infrastructure/controllers/controllers.module';
@@ -6,6 +8,7 @@ import { UsecasesProxyModule } from './infrastructure/usecases-proxy/usecases-pr
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     DatabaseModule,
     HttpCustomModule,
     UsecasesProxyModule,
