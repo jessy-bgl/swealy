365479c20baa7a0df758b831ca05dfd15fa7881c
Jessy BOGALHO <j.bogalho@m2mfactory.fr>
2022-01-11 23:07:44 +0100

    pwa boilerplate added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,8 +7,10 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import "./index.css";
 import "./i18n/i18n.js";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider as AppThemeProvider } from "./services/stores/theme/ThemeProvider";
+
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
 const queryClient = new QueryClient();
 queryClient.setDefaultOptions({
@@ -29,6 +31,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
