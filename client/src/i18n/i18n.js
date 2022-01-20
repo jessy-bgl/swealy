818ca5b944f857583016f632541bee3e72a6a700
Jessy BOGALHO <j.bogalho@m2mfactory.fr>
2022-01-20 23:36:17 +0100

    english traductions added

--- client/src/i18n/i18n.js
+++ client/src/i18n/i18n.js
@@ -1,12 +1,19 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
+
 import commonFR from "./fr/common.json";
 import navigationFR from "./fr/navigation.json";
 import settingsFR from "./fr/settings.json";
 import exchangeFR from "./fr/exchange.json";
 import dcaFR from "./fr/dca.json";
 import transactionFR from "./fr/transaction.json";
+
+import commonEN from "./en/common.json";
+import navigationEN from "./en/navigation.json";
 import settingsEN from "./en/settings.json";
+import exchangeEN from "./en/exchange.json";
+import dcaEN from "./en/dca.json";
+import transactionEN from "./en/transaction.json";
 
 export const defaultNS = "common";
 
@@ -20,7 +27,12 @@ export const resources = {
     dca: dcaFR,
   },
   en: {
+    common: commonEN,
+    navigation: navigationEN,
     settings: settingsEN,
+    exchange: exchangeEN,
+    transaction: transactionEN,
+    dca: dcaEN,
   },
 };
 
