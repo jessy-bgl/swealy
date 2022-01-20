f8435a71955fedd1506b23ebb60f1f687a09ead3
Jessy BOGALHO <j.bogalho@m2mfactory.fr>
2022-01-20 14:37:17 +0100

    local production docker files done

--- database/init.js
+++ database/init.js
@@ -2,11 +2,11 @@
 let db = connect("localhost:27017");
 
 // create the db
-db = db.getSiblingDB("crypto-dca");
+db = db.getSiblingDB("cryptodca");
 
 // create db admin user
 db.createUser({
   user: "admin",
-  pwd: "admin_passwd",
-  roles: [{ role: "readWrite", db: "crypto-dca" }],
+  pwd: "cryptodcaAdminPasswd",
+  roles: [{ role: "readWrite", db: "cryptodca" }],
 });
--- server/src/infrastructure/config/constants.ts
+++ server/src/infrastructure/config/constants.ts
@@ -1,5 +1,5 @@
 const HTTP_PORT = process.env.HTTP_PORT || 5000;
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://db/crypto-dca';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://db/cryptodca';
 
 export { HTTP_PORT, MONGO_URI };
